af63c5dc134fcac69393401f19d6a17bd243c769
kalebkb996 <kalebkb996@gmail.com>
2024-07-25 13:41:40 +0000

    added paginations for applicants list, created response for some types of messages

--- employer_bot.py
+++ employer_bot.py
@@ -22,13 +22,14 @@ token = getenv('EMPLOYER_BOT_TOKEN')
 app = ApplicationBuilder().token(token).build()
 
 employer_registered = False
+chosen_language = "English"
+messages_per_page = 5
 # Suggested code may be subject to a license. Learn more: ~LicenseLog:1655545878.
 
 # print(data)
 
 
 def send_action(action):
-    """Sends `action` while processing func command."""
     def decorator(func):
         @wraps(func)
         async def command_func(update, context, *args, **kwargs):
@@ -74,37 +75,79 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def settings(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    settings_list = [[InlineKeyboardButton("Choose Language", callback_data="0")]]
+    settings_list = [[InlineKeyboardButton("Choose Language", callback_data="handle_choose_language")]]
     settings = InlineKeyboardMarkup(settings_list)
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="⚙️settings",
+        text="⚙️ Settings",
         reply_markup=settings
     )
 
 @send_action(ChatAction.TYPING)
 async def callback_query_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
-    # await context.bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
     if query.data == "delete_job_btn":
         await context.bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
+    
     elif query.data == "list_applicants":
         job = query.message.text
-        await context.bot.send_message(chat_id=query.message.chat_id, text=f"Applicants For Job\n-------------------\n{job}")
-        for applicant in applicants:
-            print(applicant["username"])
-            await context.bot.send_message(
-                chat_id=query.message.chat_id,
-                text=applicant_message_builder(applicant),
-                parse_mode="HTML",
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Contact", url=f"https://t.me/{applicant['username'][1:]}")]])
-            )
+        seen = None
+        if "ended_at" not in context.user_data:
+            seen = 0
+            await context.bot.send_message(chat_id=query.message.chat_id, text=f"Applicants For Job\n-------------------\n{job}")
+        
+        else:
+            seen = context.user_data['ended_at']
+        seen_per_session = 0
+        for applicant in applicants[seen:]:
+            if seen_per_session < messages_per_page:
+                await context.bot.send_message(
+                    chat_id=query.message.chat_id,
+                    text=applicant_message_builder(applicant),
+                    parse_mode="HTML",
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Contact", url=f"https://t.me/{applicant['username'][1:]}")]])
+                )
+                seen_per_session += 1
+                seen += 1
+            else:
+                context.user_data["ended_at"] = seen
+                await context.bot.send_message(
+                    chat_id=query.message.chat_id,
+                    text=f"shown {seen} out of {len(applicants)} applicants",
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("Show More", callback_data="list_applicants")],
+                    ])
+                )
+                break
+
+    elif query.data == "cancel_language_selection":
+        #this is temporary it must return back to the previous oprtions of the settings
+        await context.bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
+
+
+
+    elif query.data == "handle_choose_language":
+        languages = ["English", "Amharic"]
+        await query.answer()
+        # languages_list = [[InlineKeyboardButton(f"{language} {'(currently selected)' if chosen_language == language else ''}", callback_data=f"set_language_{language}")] for language in languages]
+        languages_list = [
+            [InlineKeyboardButton("English", callback_data="0")],
+            [InlineKeyboardButton("Cancel", callback_data="cancel_language_selection")]
+        ]
+        await context.bot.edit_message_text(
+            text="Choose a language:",
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            reply_markup=InlineKeyboardMarkup(languages_list)
+        )
 
 async def reply_keyboard_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message.text == "Post a Job":
         # Handle post job request
         if employer_registered:
-            miniapp_form_btn = [[InlineKeyboardButton("Open Job Post Form", callback_data="1")]]
+            miniapp_form_btn = [
+                [InlineKeyboardButton(text="Open Job Post Form", web_app=WebAppInfo(url="https://d084-35-205-187-182.ngrok-free.app/job-post-form"))]
+            ]
             reply_markup = InlineKeyboardMarkup(miniapp_form_btn)
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
@@ -117,7 +160,7 @@ async def reply_keyboard_handler(update: Update, context: ContextTypes.DEFAULT_T
                 reply_markup=InlineKeyboardMarkup.from_button(
                     InlineKeyboardButton(
                         text="Open Registration Form",
-                        web_app=WebAppInfo(url="https://cea3-34-140-89-220.ngrok-free.app/#company-registration")
+                        web_app=WebAppInfo(url="https://d084-35-205-187-182.ngrok-free.app")
                     )
                 )
             )
@@ -125,7 +168,6 @@ async def reply_keyboard_handler(update: Update, context: ContextTypes.DEFAULT_T
     elif update.message.text == "Posted Jobs":
    
         for job in data:
-            # applicants_count = random.randint(1, 100)
             message = jobdetail_message_builder(job)
             await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
             await context.bot.send_message(
@@ -140,22 +182,43 @@ async def reply_keyboard_handler(update: Update, context: ContextTypes.DEFAULT_T
                 ])
             )
 
-    # elif update.message.text == "Guides":
-    #     # Handle guides request
+    elif update.message.text == "Guides":
+        # Handle guides request
+        await context.bot.send_message(text="Not yet implemented")
+
+
+
     elif update.message.text == "Give FeedBack":
+        context.user_data['waiting_for_feedback'] = True
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="we will appreciate your feedback, it helps us improve the platform, tell us what you think about it and what needs to improve"
         )
 
+    elif 'waiting_for_feedback' in context.user_data:
+        # Handle the feedback message
+        feedback = update.message.text
+        print(f"Received feedback from {update.effective_user.username}: {feedback}")  # Store or process the feedback as needed
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Thank you for your feedback! We'll use it to make our platform even better."
+        )
+        del context.user_data['waiting_for_feedback']  # Reset the flag
 
 
-    # else:
-    #     await update.message.reply_text("Invalid command")
+async def handle_web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    data = update.effective_message.web_app_data.data
+    await update.effective_message.reply_text(f"Received data: {data}")
+    result = {"status": "success"}
+    await context.bot.answer_web_app_query(
+        web_app_query_id=update.effective_message.web_app_data.id,
+        result=result,
+    )
 
 
 messsage_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), reply_keyboard_handler)
-
+# app.add_handler(MessageHandler(filters.ALL, log_all_updates))
+app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, handle_web_app_data))
 app.add_handler(messsage_handler)
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("settings", settings))
